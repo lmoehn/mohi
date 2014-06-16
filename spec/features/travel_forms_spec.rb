7b544adf40a4e104b6d0f95249ca9f8cbd8d2e81
lmoehn <lmoehn@gmail.com>
2014-06-16 15:09:49 -0600

    Add required to travel_form view fields
    Corrected formatting for travel_forms new view
    Add test for changing travel_forms approve_date field

--- spec/features/travel_forms_spec.rb
+++ spec/features/travel_forms_spec.rb
@@ -5,12 +5,12 @@ feature 'manage pdf forms' do
     visit '/travel_forms/new'
     fill_in 'player_name', with: 'player Jane'
     fill_in 'parent_name', with: 'parent Doe'
-    fill_in 'approve_date', with: 'June 14, 2014'
+    fill_in 'approve_date', with: 'June 1, 2014'
 
     click_on 'Approve'
 
     expect(find_field('player_name').value).to eq 'player Jane'
     expect(find_field('parent_name').value).to eq 'parent Doe'
-    expect(find_field('approve_date').value).to eq 'June 14, 2014'
+    expect(find_field('approve_date').value).to eq 'June 1, 2014'
   end
 end
