b6938ce78ff3d11f18dcc4c1c4460f2e94380720
lmoehn <lmoehn@gmail.com>
2014-06-16 14:28:21 -0600

    Take out params variable assignments which were not needed

--- app/controllers/travel_forms_controller.rb
+++ app/controllers/travel_forms_controller.rb
@@ -9,14 +9,12 @@ class TravelFormsController < ApplicationController
   end
 
   def create
-    @player_name = params["player_name"]
-    @parent_name = params["parent_name"]
+    @player_name_no_whitespace = params["player_name"].gsub(/\s+/, "")
 
-    @player_name_no_whitespace = @player_name.gsub(/\s+/, "")
-
-    html = render_to_string(action: "new")
-    kit = PDFKit.new(html, :page_size => 'Letter')
+    html = render_to_string(action: "show")
+    kit = PDFKit.new(html, page_size: "Letter")
     file = kit.to_file("#{Rails.root}/public/pdfs/travel_form_#{@player_name_no_whitespace}.pdf")
+
     render :show
   end
 end
